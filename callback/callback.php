88f871c8a9fe8ce9cf03f593f578946a196d8c58
Aveline Swan <i@xswan.net>
2014-06-08 16:19:28 +0800

    fix hbf path

--- callback/callback.php
+++ callback/callback.php
@@ -5,5 +5,5 @@ $_GET['ali_type_r'] = 'callback';
 error_reporting( 0 );
 session_name('SESSID' . substr(md5($_SERVER['SERVER_NAME']), -4));
 session_start();
-include('../../../../hbf/bootstrap.php');
+include('../../../../../hbf/bootstrap.php');
 FrontController::init( 'module' );
